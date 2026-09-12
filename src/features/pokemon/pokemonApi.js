0f3f338f28851f01629388d85fd9324ebd492cf2
Lautaro Coria <lautaronahuel.coria@gmail.com>
2026-09-12 16:11:26 -0300

    feat: infinite scroll by clicking

--- src/features/pokemon/pokemonApi.js
+++ src/features/pokemon/pokemonApi.js
@@ -1,9 +1,16 @@
 import { pokeApi } from '../../api/pokeApi'
 
+const PAGE_SIZE = 20
+
 export const pokemonApi = pokeApi.injectEndpoints({
   endpoints: (builder) => ({
     getPokemonList: builder.query({
-      query: () => 'pokemon?limit=20&offset=0',
+    	query: (page = 0) => `pokemon?limit=${PAGE_SIZE}&offset=${page * PAGE_SIZE}`,
+      serializeQueryArgs: ({ endpointName }) => endpointName,
+      merge: (currentCache, newItems) => {
+        currentCache.results.push(...newItems.results)
+      },
+      forceRefetch: ({ currentArg, previousArg }) => currentArg !== previousArg,
     }),
   }),
 })
