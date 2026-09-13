5b77d02542bf22feb1e4c82e72a4b72d3bcd3f67
Lautaro Coria <lautaronahuel.coria@gmail.com>
2026-09-12 21:50:12 -0300

    feat: add basic styles to the PokemonCard component

--- src/features/pokemon/PokemonCard.jsx
+++ src/features/pokemon/PokemonCard.jsx
@@ -1,18 +1,36 @@
 import { useGetPokemonDetailQuery } from './pokemonApi'
 import { extractIdFromUrl } from './utils'
+import styles from './pokemonCard.module.css'
 
 function PokemonCard({ url }) {
   const id = extractIdFromUrl(url)
   const { data, isLoading, isError } = useGetPokemonDetailQuery(id)
 
-  if (isLoading) return <li>Cargando Pokemon Detail</li>
-  if (isError) return <li>Error al cargar</li>
+  if (isLoading) return <li className={styles.card}>Cargando Pokemon Detail</li>
+  if (isError) return <li className={styles.card}>Error al cargar</li>
 
   return (
-    <li>
-      <img src={data.sprites.other.showdown.front_default} alt={data.name} width={96} height={96} />
-      <p>#{data.id} {data.name}</p>
-      <p>{data.types.map((t) => t.type.name).join(', ')}</p>
+    <li className={styles.card}>
+      <img
+        className={styles.sprite}
+        src={data.sprites.other.showdown.front_default}
+        alt={data.name}
+        width={96}
+        height={96}
+      />
+      <p className={styles.number}>#{data.id}</p>
+      <p className={styles.name}>{data.name}</p>
+      <ul className={styles.badges}>
+        {data.types.map(({ type }) => (
+          <li
+            key={type.name}
+            className={styles.badge}
+            style={{ '--badge-color': `var(--type-${type.name})` }}
+          >
+            {type.name}
+          </li>
+        ))}
+      </ul>
     </li>
   )
 }
