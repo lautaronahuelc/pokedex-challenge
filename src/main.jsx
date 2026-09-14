eba93e384b29552fd9c7dd464636e89e0c359bc1
Lautaro Coria <lautaronahuel.coria@gmail.com>
2026-09-14 01:08:29 -0300

    feat: persist favorites in localStorage using redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { BrowserRouter } from 'react-router-dom'
 
 import './styles/global.css'
 import App from './App.jsx'
-import { store } from './app/store.js'
+import { store, persistor } from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
